cc0186b153a33f95cd8dbce70889f9cbb248f17b
Armando Galeana <a@digitalethos.consulting>
2025-09-03 20:13:08 -0600

    cleansed git repo and updated .gitignore

--- app.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.title("Hello streamlit")
-st.write("This is my first Streamlit app")
-
-number = st.slider("Pick a number", min_value=0, max_value=100, value=50)
-st.write(f"You picked: {number}")
